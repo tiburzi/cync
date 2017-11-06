230d9a5c39df84b6de2986f4462fc4810ac22d7b
Omar Shehata <shehat1@stolaf.edu>
2017-11-06 09:29:16 -0600

    Changed name of states

--- code/preload.js
+++ code/preload.js
@@ -2,10 +2,10 @@ var preload = function(game) {};
  
 preload.prototype = {
 	preload: function() { 
-        this.game.load.image('test_image', 'images/test_image.png');
+        this.game.load.image('test_image', 'assets/images/test_image.png');
 	},
     
   	create: function(){
-		this.game.state.start('mainState');
+		this.game.state.start('main');
 	}
 }
