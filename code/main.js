bcb24085c7e23cfb8729eb2c6fbc807ab709dd2f
Jon <jontiburzi@gmail.com>
2017-11-09 18:09:49 -0600

    Renamed Circles to Orbits

--- code/main.js
+++ code/main.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
     // Our 'global' variables defined up here so they're accessible everywhere below
     var two;
-    var Circles = [];
+    var Orbits = [];
     var Notes = [];
     var RADIUS_SNAP = 5;
 
@@ -16,33 +16,36 @@ window.onload = function() {
         two = new Two(params).appendTo(elem);
     }
     
-    function CreateCircle(radius){
-        /* This should create a circle that: 
+    function CreateOrbit(radius){
+        /* This should create a orbit that: 
             - Can be resized by dragging
             - Can be destroyed by dragging to the center
             - Probably has other properties associated with it later (like dragging a sample onto it)
         */
         var X = two.width / 2;
         var Y = two.height / 2
-        var circle = two.makeCircle(X,Y, radius);
-        circle.fill = 'none';
-        circle.stroke = '#6b6b6b';
-        circle.linewidth = 6;
-        circle.radius = radius; //Just for keeping track of the radius in our own application
+        var orbit = two.makeCircle(X,Y, radius);
+        orbit.fill = 'none';
+        orbit.stroke = '#6b6b6b';
+        orbit.linewidth = 6;
+        orbit.radius = radius; //Just for keeping track of the radius in our own application
 
         $(document).ready(function() {
-            addInteractionDrag(circle);
+            addInteractionDrag(orbit);
         });
 
-        Circles.push(circle);
+        Orbits.push(orbit);
 
-        circle.update = function(){
-            // For updating anything about the circle
+        orbit.update = function() {
+            // For updating anything about the orbit
             this.trigger.update();
         }
-<<<<<<< HEAD
         
-        circle.onDrag = function (e, offset, localClickPos) {
+//What is this ' <<<<<<< HEAD ============ >>>>>>> 574389758463450348756435708963456 ' thing?
+        
+//<<<<<<< HEAD
+        
+        orbit.onDrag = function (e, offset, localClickPos) {
             var x = e.clientX - offset.x - localClickPos.x;
             var y = e.clientY - offset.y - localClickPos.y;
             var point = {x:e.clientX,y:e.clientY};
@@ -51,37 +54,38 @@ window.onload = function() {
 
             var newRadius = Math.round(dist / RADIUS_SNAP) * RADIUS_SNAP;
 
+            this.radius = newRadius;
             _.each(this.vertices, function(v) {
                 v.setLength(newRadius);
             });
         }
         
-=======
+//=======
 
-        // Create a triangle trigger for this circle
+        // Create a triangle trigger for this orbit
         var size = 15;
         var triggerX = X;
-        var triggerY = Y-radius-size - circle.linewidth/2;
+        var triggerY = Y-radius-size - orbit.linewidth/2;
         var trigger = two.makePolygon(triggerX,triggerY, size);
         trigger.fill = 'orangered';
         trigger.stroke = 'none';
         trigger.rotation = Math.PI;
-        trigger.circle = circle;
-        circle.trigger = trigger;
+        trigger.orbit = orbit;
+        orbit.trigger = trigger;
 
         trigger.update = function(){
-            // Move trigger to the edge of the circle based on the rotation 
-            var offset = this.circle.radius + size + this.circle.linewidth/2
+            // Move trigger to the edge of the orbit based on the rotation 
+            var distance = this.orbit.radius + size + this.orbit.linewidth/2
             var angle = this.rotation + Math.PI/2;
-            this.translation.x = X + Math.cos(angle) * offset;
-            this.translation.y = Y + Math.sin(angle) * offset ;
+            this.translation.x = X + Math.cos(angle) * distance;
+            this.translation.y = Y + Math.sin(angle) * distance ;
 
-            this.rotation += 0.1 * (1 - (this.circle.radius / 300));
+            this.rotation += 0.1 * (1 - (this.orbit.radius / 300));
         }
 
 
->>>>>>> 79f3fda5ca421faf1a374cee9d2107854a1cb054
-        return circle;
+//>>>>>>> 79f3fda5ca421faf1a374cee9d2107854a1cb054
+        return orbit;
     }
     
     function CreateNote(radius){
@@ -95,7 +99,7 @@ window.onload = function() {
         note.linewidth = 0;
 
         $(document).ready(function() {
-            addInteractionDrag(circle);
+            addInteractionDrag(orbit);
         });
 
         Notes.push(note);
@@ -109,8 +113,8 @@ window.onload = function() {
     Init();
     
     
-    CreateCircle(50);
-    CreateCircle(100);
+    CreateOrbit(50);
+    CreateOrbit(100);
 
     
     //interactivity code from https://two.js.org/examples/advanced-anchors.html
@@ -122,21 +126,7 @@ window.onload = function() {
         var drag = function(e) {
           e.preventDefault();
           shape.onDrag(e, offset, localClickPos);
-
-          /*var x = e.clientX - offset.x - localClickPos.x;
-          var y = e.clientY - offset.y - localClickPos.y;
-          var point = {x:e.clientX,y:e.clientY};
-          var center = {x:two.width / 2,y:two.height / 2};
-          var dist = Math.sqrt(Math.pow(point.x - center.x,2) + Math.pow(point.y - center.y,2));
-
-          var newRadius = Math.round(dist / RADIUS_SNAP) * RADIUS_SNAP;
-
-          shape.radius = newRadius;
-          _.each(shape.vertices, function(v) {
-             v.setLength(newRadius);
-          });*/
-
-
+            
         };
         var touchDrag = function(e) {
           e.preventDefault();
@@ -200,8 +190,8 @@ window.onload = function() {
         // Two's own update
         two.update();
         // Our own update goes here
-        for(var i=0;i<Circles.length;i++)
-            Circles[i].update();
+        for(var i=0;i<Orbits.length;i++)
+            Orbits[i].update();
 
         // Ask the browser to run this on the next frame please
         requestAnimationFrame( update );
